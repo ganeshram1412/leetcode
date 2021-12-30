b7f377e2c7d8d51a136d90658aca1904a3d6224f
Ganesh Ram Rajakumar <ganeshram1412@gmail.com>
2021-12-30 14:38:41 +0530

    Adding Array Concatenation problem

---
+++ DSA-Bootcamp-Java/Arrays/ArrayConcatenation.java
@@ -0,0 +1,13 @@
+import java.util.logging.Logger;
+
+// leetcode url: https://leetcode.com/problems/concatenation-of-array
+
+// level: easy
+
+class Solution {
+    private static final Logger log = Logger.getLogger(Solution.class.getName());
+
+    public int[] getConcatenation(int[] nums) {
+
+    }
+}
