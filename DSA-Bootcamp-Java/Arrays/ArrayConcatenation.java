e13aa55b17c89461ca743f80a93879af4d804552
Ganesh Ram Rajakumar <ganeshram1412@gmail.com>
2021-12-31 11:45:24 +0530

    leetcode array concatenation problem submitted

--- DSA-Bootcamp-Java/Arrays/ArrayConcatenation.java
+++ DSA-Bootcamp-Java/Arrays/ArrayConcatenation.java
@@ -1,13 +1,27 @@
+package Arrays;
+
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 // leetcode url: https://leetcode.com/problems/concatenation-of-array
 
 // level: easy
 
-class Solution {
-    private static final Logger log = Logger.getLogger(Solution.class.getName());
+class SolutionArrayConcatenation {
+    private static final Logger log = Logger.getLogger(SolutionArrayConcatenation.class.getName());
 
-    public int[] getConcatenation(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4 };
+        log.info(Arrays.toString(getConcatenation(nums)));
+    }
 
+    public static int[] getConcatenation(int[] nums) {
+        int length = nums.length;
+        int[] result = new int[length * 2];
+        for (int i = 0; i < length; i++) {
+            result[i] = nums[i];
+            result[i + length] = nums[i];
+        }
+        return result;
     }
 }
