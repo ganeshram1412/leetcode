a4ee34a3c27007c82c8c722258989ab52b071796
Ganesh Ram Rajakumar <ganeshram1412@gmail.com>
2022-01-02 16:52:10 +0530

    Panagram program

---
+++ DSA-Bootcamp-Java/Arrays/ArrayPangram.java
@@ -0,0 +1,34 @@
+package Arrays;
+//leetcode url:https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+
+//level: easy
+
+class SolutionPangram {
+    public static void main(String[] args) {
+        // String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        String sentence = "qklccnqeicrabxpggieplwjhakurwwhxbugbryvhazoofifidzvxczmpdjfcyiuhqyedxhzexvpitxknjogpetvgxeqrjuuxzzfblhmhbgibocbhtcbgyxzchlawvnhczlecsrioapggorouzcputqsxhvoxbqxxydiumxwg";
+        System.out.println(checkIfPangram2(sentence));
+    }
+
+    public static boolean checkIfPangram(String sentence) {
+        if (sentence.length() < 26) {
+            return false;
+        } else {
+            char[] sentencechar = sentence.toCharArray();
+            HashMap<Integer, Boolean> valueHashMap = new HashMap<>();
+            for (int i = 0; i < sentencechar.length; i++) {
+                int value = (int) sentencechar[i];
+                valueHashMap.put(value, true);
+            }
+            if (valueHashMap.size() != 26) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
