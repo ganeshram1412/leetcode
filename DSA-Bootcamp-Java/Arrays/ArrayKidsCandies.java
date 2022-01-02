e15b9c12e75f80d2ede7fd199ef37983d5801466
Ganesh Ram Rajakumar <ganeshram1412@gmail.com>
2022-01-02 15:10:11 +0530

    Adding kids candies problem :q

---
+++ DSA-Bootcamp-Java/Arrays/ArrayKidsCandies.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SolutionKidsCandies {
+    public static void main(String[] args) {
+        int[] candies = { 2, 3, 5, 1, 3 };
+        List<Boolean> result = kidsWithCandies(candies, 3);
+        for (boolean value : result) {
+            System.out.println(value);
+        }
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        int max = 0;
+        for (int i = 0; i < candies.length; i++) {
+            int totalCandiesForKid = candies[i] + extraCandies;
+            if (totalCandiesForKid >= max) {
+                max = totalCandiesForKid;
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+
+        }
+        return result;
+    }
+
+}
