535212745aefa4c96f54d9b0e68bccfd8978adbd
Ganesh Ram Rajakumar <ganeshram1412@gmail.com>
2022-01-08 22:26:49 +0530

    Flipping image

--- DSA-Bootcamp-Java/Arrays/FlippingImage.java
+++ DSA-Bootcamp-Java/Arrays/FlippingImage.java
@@ -1,32 +1,33 @@
 package Arrays;
 
+import java.util.Arrays;
+
 //leetcode easy
 //leetcode url https://leetcode.com/problems/flipping-an-image/
 class FlippingImage {
     public static void main(String[] args) {
-        int[][] image = { { 1, 1, 0 }, { 1, 0, 1 }, { 0, 0, 1 } };
-        flipAndInvertImage(image);
+        int[][] image = { { 1, 1, 0 }, { 1, 0, 1 }, { 0, 0, 0 } };
+        int[][] result = flipAndInvertImage(image);
+        for (int i = 0; i < result.length; i++) {
+
+            for (int j = result[i].length - 1; j >= 0; j--) {
+                System.out.println("result is " + result[i][j]);
+            }
+        }
     }
 
     public static int[][] flipAndInvertImage(int[][] image) {
-        int[][] result = new int[image.length][image.length];
-        for (int i = image.length - 1; i >= 0; i--) {
-            int index1 = 0;
-            int index2 = 0;
+
+        for (int i = 0; i < image.length; i++) {
 
             for (int j = image[i].length - 1; j >= 0; j--) {
-
                 if (image[i][j] == 0) {
-                    result[index1][index2] = 1;
-
+                    image[i][j] = 1;
                 } else {
-                    result[index1][index2] = 0;
-
+                    image[i][j] = 0;
                 }
-                ++index2;
             }
-            ++index1;
         }
-        return result;
+        return image;
     }
 }
