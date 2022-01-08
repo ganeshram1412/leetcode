02d23ff4d773a93bb956e2bca0727f268ffb2746
Ganesh Ram Rajakumar <ganeshram1412@gmail.com>
2022-01-08 22:38:08 +0530

    Matrix sum :Q

---
+++ DSA-Bootcamp-Java/Arrays/MatrixSum.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+class MatrixAddition {
+    public static void main(String[] args) {
+
+    }
+
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (i == j || i + j == mat.length - 1) {
+                    sum = sum + mat[i][j];
+                }
+            }
+
+        }
+        return sum;
+    }
+}
